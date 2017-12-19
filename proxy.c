1ea9341876af36eaa784dc707311edd412c264b0
lsy <lishunyao97@gmail.com>
2017-12-19 09:58:58 +0800

    remove cache

--- proxy.c
+++ proxy.c
@@ -1,43 +1,10 @@
-/*
- *  Proxylab
- *
- *  Auther: Tianyu Chen
- *  Andrew ID: tianyuc
- *  Email: tianyuc@andrew.cmu.edu
- *
- *  [Brief Description]: 
- *    This is a basic proxy, which acts as a server when connecting to a 
- *  client and acts as a client when connecting to remote web servers. And 
- *  it can deal with multiple concurrent requests. Finally, a cache with LRU
- *  rule is added to improve the function of this proxy.
- *
- *    The proxy works well on the following pages: 
- *      – http://www.cs.cmu.edu/˜213
- *      – http://csapp.cs.cmu.edu
- *      – http://www.cmu.edu
- *      – http://www.amazon.com
- *      - http://www.youtube.com
- *    
- *    Some features in this proxy:
- *      - Robust I/O  
- *      - Block SIGPIPE signals
- *      - LRU eviction rule
- *      - lock proper threads under concurrent requests
- *      
- */
-
 #include <stdio.h>
-
-/* Recommended max cache and object sizes */
-#define MAX_CACHE_SIZE 1049000
-#define MAX_OBJECT_SIZE 102400
-
+#include "csapp.h"
 /* You won't lose style points for including these long lines in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
 
-#include "csapp.h"
 
 void doit(int fd);
 void read_requesthdrs(rio_t *rp);
@@ -46,37 +13,27 @@ void clienterror(int fd, char *cause, char *errnum,
 		         char *shortmsg, char *longmsg);
 int powerten(int i);
 void proxy_init(void);
-void init_cache(void);
-void *thread(void *vargp);
-static void update_use(int *cache_use, int current, int len);
-static int load_cache(char *tag, char *response);
 
-static void save_cache(char *tag, char *response);
+void *thread(void *vargp);
+
 static void request_hdr(char *buf, char *buf2ser, char *hostname);
 
-struct cache_line
-{
-    int valid;
-    char *tag;
-    char *block;
-};
 
-struct cache_set
-{
-    struct cache_line *line;
-    int *use;
-};
-
-struct cache
-{
-    struct cache_set *set;
-};
-
-static struct cache cache;
 
 // global variables
 sem_t mutex;
 static int set_num, line_num;
+
+FILE* log;
+float alpha;
+int listen_port;
+char* fake_ip;
+char* dns_ip;
+int dns_port;
+char* www_ip;
+char* server_name="video.pku.edu.cn";
+
+
 
 int main(int argc, char **argv) 
 {
@@ -91,11 +48,14 @@ int main(int argc, char **argv)
     pthread_t tid;
 
     /* Check command line args */
-    if (argc != 2) {
-	    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    if (argc != 4) {
+	    fprintf(stderr, "usage: %s <listen-port> <fake-ip> <www-ip>\n", argv[0]);
 	    exit(1);
     }
-
+    strcpy(fake_ip, argv[2]);
+    strcpy(www_ip, argv[3]);
+    printf("fake_ip = %s\n",fake_ip);
+    printf("www_ip = %s\n",www_ip);
     proxy_init();
     listenfd = Open_listenfd(argv[1]);
     while (1) {
@@ -104,7 +64,6 @@ int main(int argc, char **argv)
 	    *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-
         pthread_create(&tid, NULL, thread, connfd);
     }
     return 0;
@@ -112,102 +71,12 @@ int main(int argc, char **argv)
 
 /*
  * proxy_init
- * initialize the whole proxy including cache
+ * initialize the whole proxy
  */
 void proxy_init()
 {
     sem_init(&mutex, 0, 1);
-    set_num = 1;
-    line_num = 10;
-    init_cache();
-}
-
-/*
- * init_cache 
- * initialize the cache, malloc space for the cache
- */
-void init_cache()
-{
-    int i, j;
-    cache.set = malloc(sizeof (struct cache_set) * set_num);
-    for (i = 0; i < set_num; i++)
-    {
-        cache.set[i].line = malloc(sizeof(struct cache_line) * line_num);
-        cache.set[i].use = malloc(sizeof(int) * line_num);
-       for (j = 0; j < line_num; j++)
-       {
-           cache.set[i].use[j] = j;
-           cache.set[i].line[j].valid = 0;
-           cache.set[i].line[j].tag = malloc(MAXLINE);
-           cache.set[i].line[j].block = malloc(MAX_OBJECT_SIZE);
-       } 
-    }
-}
-
-/* 
- * update_use - record cache usage condition for eviction
- * record the recent use of cache store in the array 'use', LRU rule
- */
-static void update_use(int *cache_use, int current, int len)
-{
-    int i, j;
-    for(i = 0; i < len; i++)
-    {
-        if(cache_use[i] == current) {
-             break;
-        }
-    }
-    for(j = i; j > 0; j--)
-    {
-        cache_use[j] = cache_use[j - 1];
-    }                               
-//#include <string.h>
-    cache_use[0] = current;
-}
-
-/*
- * load_cache - load data from cache
- * search desired cache and buffer the data in response
- */
-static int load_cache(char *tag, char *response) 
-{
-    int index, i;
-    index = 0;
-    for (i = 0; i < line_num; i++) {
-        if(cache.set[index].line[i].valid == 1 && 
-          (strcmp(cache.set[index].line[i].tag, tag) == 0))
-        {
-            P(&mutex);
-            update_use(cache.set[index].use, i, line_num);
-            V(&mutex);
-            strcpy(response, cache.set[index].line[i].block);
-            break;
-        }
-    }
-    if (i == line_num) {
-        return 0;
-    }
-    else {
-        return 1;
-    }
-}
-
-/* 
- * save_cache - save data from server in cache
- * copy response and tag into cache 
- */ 
-static void save_cache(char *tag, char *response)
-{
-    int index, eviction;
-    index = 0;
-    eviction = cache.set[index].use[line_num - 1];
-    strcpy(cache.set[index].line[eviction].tag, tag);
-    strcpy(cache.set[index].line[eviction].block, response);
-//#include <string.h>
-    if (cache.set[index].line[eviction].valid == 0) {
-        cache.set[index].line[eviction].valid = 1;
-    }
-    update_use(cache.set[index].use, eviction, line_num);;
+    
 }
 
 /*
@@ -222,7 +91,7 @@ void doit(int fd)
     int *port;
     char port2[10];
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char cache_buf[MAX_OBJECT_SIZE];// store cache data stuff
+    
     char filename[MAXLINE];         // client request filename
     char hostname[MAXBUF];          // client request hostname
     char buf2ser[MAXLINE];          // proxy to server
@@ -240,7 +109,7 @@ void doit(int fd)
     memset(method, 0, sizeof(method));
     memset(buf, 0, sizeof(buf));
     memset(version, 0, sizeof(version));
-    memset(cache_buf, 0, sizeof(cache_buf)); 
+    
 
     // step1: obtain request from client and parse the request
     Rio_readinitb(&rio, fd);
@@ -269,58 +138,67 @@ void doit(int fd)
     strcpy(filename, uri);
     sprintf(buf2ser, "%s %s %s\r\n", method, filename, version);
     printf("proxy to server: %s\n", buf2ser);
-
+    if(strcmp(hostname,))
     // request header
     request_hdr(buf, buf2ser, hostname);
-    
-    // check cache first
-    if (load_cache(uri, cache_buf) == 1) {
-        printf("Hit!\n");
-        if (rio_writen(fd, cache_buf, sizeof(cache_buf)) < 0) {
-            fprintf(stderr, "Error: cache load!\n");
-            return;
-        }
-        memset(cache_buf, 0, sizeof(cache_buf));
-    }
-    else {   
-    // if cache miss then forward the request to server
+
     // step2 : from proxy to server
-        sprintf(port2, "%d", *port);
-        if((serverfd = open_clientfd(hostname, port2)) < 0)
-        {
-            fprintf(stderr, "open server fd error\n");
-            return;
-        }
-
-        Rio_readinitb(&rio_ser, serverfd);
-
-        // send request to server
-        Rio_writen(serverfd, buf2ser, strlen(buf2ser));
-
-        // step3: recieve the response from the server and save data in cache
-        memset(cache_buf, 0, sizeof(cache_buf));
-        object_len = 0;
-
-        while ((len = rio_readnb(&rio_ser, ser_response, 
-                sizeof(ser_response))) > 0) {
-
-            Rio_writen(fd, ser_response, len);
-
-            strcat(cache_buf, ser_response);
-            object_len += len;
-            memset(ser_response, 0, sizeof(ser_response)); 
-        }    
-        if (object_len <= MAX_OBJECT_SIZE)
-        {
-            P(&mutex);
-            save_cache(uri, cache_buf);
-            V(&mutex);
-        }
-        close(serverfd);
+    sprintf(port2, "%d", 8080);
+    if((serverfd = open_clientfd_bind_fakeip(hostname, port2, fake_ip)) < 0)
+    {
+        fprintf(stderr, "open server fd error\n");
+        return;
     }
+
+    Rio_readinitb(&rio_ser, serverfd);
+
+    // send request to server
+    Rio_writen(serverfd, buf2ser, strlen(buf2ser));
+
+    // step3: recieve the response from the server
+
+    while ((len = rio_readnb(&rio_ser, ser_response,
+            sizeof(ser_response))) > 0) {
+
+        Rio_writen(fd, ser_response, len);
+        memset(ser_response, 0, sizeof(ser_response));
+    }
+    close(serverfd);
+    
 }
 /* $end doit */
-
+/* $begin open_clientfd_bind_fakeip */
+int open_clientfd_bind_fakeip(char *hostname, char *port, char *fake_ip) {
+    int clientfd;
+    struct addrinfo hints, *listp, *p;
+    
+    /* Get a list of potential server addresses */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;  /* Open a connection */
+    hints.ai_flags = AI_NUMERICSERV;  /* ... using a numeric port arg. */
+    hints.ai_flags |= AI_ADDRCONFIG;  /* Recommended for connections */
+    Getaddrinfo(hostname, port, &hints, &listp);
+    
+    /* Walk the list for one that we can successfully connect to */
+    for (p = listp; p; p = p->ai_next) {
+        /* Create a socket descriptor */
+        if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+            continue; /* Socket failed, try the next */
+        
+        /* Connect to the server */
+        if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1)
+            break; /* Success */
+        Close(clientfd); /* Connect failed, try another */  //line:netp:openclientfd:closefd
+    }
+    
+    /* Clean up */
+    Freeaddrinfo(listp);
+    if (!p) /* All connects failed */
+        return -1;
+    else    /* The last connect succeeded */
+        return clientfd;
+}
+/* $end open_clientfd */
 /*
  * request_hdr - request header
  * if the request does not contain header, add request header
