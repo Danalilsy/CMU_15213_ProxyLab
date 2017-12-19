31b2c5c02055977b633653630893d491f8e6939d
lsy <lishunyao97@gmail.com>
2017-12-19 15:37:57 +0800

    1

--- proxy.c
+++ proxy.c
@@ -151,34 +151,14 @@ void doit(int fd)
 
         // step3: recieve the response from the server
         while ((len = rio_readnb(&rio_ser, ser_response,sizeof(ser_response))) > 0) {
-            //Rio_writen(fd, ser_response, len);
             strcpy(xml, ser_response);
             parse_bitrates(xml);
+            printf("xml=\n%s",xml);
+            Rio_writen(fd, ser_response, len);//sizeof
             int i;
             for(i=0;i<bitrate_cnt;i++){
                 printf("bitrate = %d\n",bitrate_array[i]);
             }
-            memset(ser_response, 0, sizeof(ser_response));
-        }
-        close(serverfd);
-        
-        ///////////////////////////////
-        
-        sprintf(buf2ser, "%s %s %s\r\n", method, uri_nolist, version);
-        printf("proxy to server: %s\n", buf2ser);
-        if ((serverfd = open_clientfd_bind_fake_ip(hostname, port2, fake_ip)) < 0){
-            fprintf(stderr, "open server fd error\n");
-            return;
-        }
-        
-        Rio_readinitb(&rio_ser, serverfd);
-        
-        // send request to server
-        Rio_writen(serverfd, buf2ser, strlen(buf2ser));
-        
-        // step3: recieve the response from the server
-        while ((len = rio_readnb(&rio_ser, ser_response,sizeof(ser_response))) > 0) {
-            Rio_writen(fd, ser_response, len);
             memset(ser_response, 0, sizeof(ser_response));
         }
         close(serverfd);
